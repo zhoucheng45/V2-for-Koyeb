93ad44a674ecd143184ef7488d5b738c487bdfd9
zhoucheng45 <178542285@qq.com>
2023-06-03 14:44:26 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,13 +12,13 @@ sed -i "s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g" /etc/
 # 伪装 v2ray 执行文件
 RELEASE_RANDOMNESS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
 mv v ${RELEASE_RANDOMNESS}
-cat config.json | base64 > config
-rm -f config.json
+# cat config.json | base64 > config
+# rm -f config.json
 
 # 如果有设置哪吒探针三个变量,会安装。如果不填或者不全,则不会安装
 # [ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ] && wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && echo '0' | ./nezha.sh install_agent ${NEZHA_SERVER} ${NEZHA_PORT} ${NEZHA_KEY}
 
 # 运行 nginx 和 v2ray
 nginx
-base64 -d config > config.json
+# base64 -d config > config.json
 ./${RELEASE_RANDOMNESS} -config=config.json
